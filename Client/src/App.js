2752a1e7de00557a7d97fb48071021d5458c98b5
KYohanaGomez <kygg.16@hotmail.com>
2023-10-03 21:12:37 -0500

    Avance1

--- Client/src/App.js
+++ Client/src/App.js
@@ -33,24 +33,7 @@ function App() {
       }
       
    }
-   // const login = (userData) => {
-   //    const { email, password } = userData;
-   //    const URL = 'http://localhost:3001/rickandmorty/login/';
-   //    axios(URL + `?email=${email}&password=${password}`)
-   //    .then(({ data }) => {
-   //       const { access } = data;
-   //       setAccess(data);
-   //       access && navigate('/home');
-   //    });
-   // }
-   // const login = (userData) =>{
-   //    if(userData.email === email && userData.password === password){
-   //       setAccess(true);
-   //       navigate('/home');
-   //    }
-
-   // }
-
+   
    useEffect(()=>{
       // eslint-disable-next-line
     !access && navigate('/')
@@ -119,3 +102,24 @@ function App() {
 }
 
 export default App;
+
+
+
+
+// const login = (userData) => {
+   //    const { email, password } = userData;
+   //    const URL = 'http://localhost:3001/rickandmorty/login/';
+   //    axios(URL + `?email=${email}&password=${password}`)
+   //    .then(({ data }) => {
+   //       const { access } = data;
+   //       setAccess(data);
+   //       access && navigate('/home');
+   //    });
+   // }
+   // const login = (userData) =>{
+   //    if(userData.email === email && userData.password === password){
+   //       setAccess(true);
+   //       navigate('/home');
+   //    }
+   // }
+
